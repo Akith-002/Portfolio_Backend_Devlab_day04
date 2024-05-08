103f23383b6619de7fd0b029b1510fcdaf1d9710
Akith-002 <chandinubaa.22@uom.lk>
2024-05-08 18:31:06 +0530

    backend-frontend integration

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 
 require("dotenv").config();
 const Project = require("./Project");
+const Blog = require("./Blog");
 // there are two endpoints yet (/ , /project)
 
 app.get("/", (req, res) => {
@@ -16,6 +17,16 @@ app.get("/projects", async (req, res) => {
   try {
     const projects = await Project.find();
     res.json(projects);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.get("/blogs", async (req, res) => {
+  // this is an endpoint
+  try {
+    const blogs = await Blog.find();
+    res.json(blogs);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
